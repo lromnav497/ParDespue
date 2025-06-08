a377de5ca632cb95e1921d607872edabe13a0ce0
lromnav497 <lromnav497@g.educaand.es>
2025-06-08 21:41:57 +0200

    Update CrearCapsula.jsx

--- frontend/src/views/CrearCapsula.jsx
+++ frontend/src/views/CrearCapsula.jsx
@@ -433,6 +433,7 @@ const CrearCapsula = () => {
               <div className="mt-4">
                 <label className="block text-white mb-2">Añadir destinatarios</label>
                 <form
+                  noValidate
                   onSubmit={e => {
                     e.preventDefault();
                     if (
@@ -460,6 +461,8 @@ const CrearCapsula = () => {
                     value={recipientEmail}
                     onChange={e => setRecipientEmail(e.target.value)}
                     className="flex-1 bg-[#1a1a4a] border border-[#3d3d9e] rounded-lg py-2 px-4 text-white"
+                    autoComplete="off"
+                    // NO pongas required aquí
                   />
                   <select
                     value={recipientRole}
